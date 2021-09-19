14fc8939b72a133f45277e13e93395b0cc8f4603
jennafritz <jennafritz24@gmail.com>
2021-09-19 18:46:20 -0400

    Basic requirements completedd, plus some fonts/styling and heart icon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import ImageComponent from './ImageComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 function App() {
+
+  const [imagesArray, setImagesArray] = useState([])
+
+  useEffect(() => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&count=10`)
+      .then(res => res.json())
+      .then(arrayOfImages => setImagesArray([...arrayOfImages]))
+    // return () => {
+    //   cleanup
+    // }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container className="homePageContainer">
+      <Row id="spacestagram" as="h1">Spacestagram</Row>
+      <Row as="h3">Solar-Powered by NASA's Astronomy Picture of the Day (APOD) API</Row>
+      {imagesArray.map(image => (
+        <ImageComponent imageData={image} />
+      ))}
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/ImageComponent.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react"
+import Card from "react-bootstrap/Card"
+import Button from 'react-bootstrap/Button'
+import Accordion from 'react-bootstrap/Accordion'
+import { Container, Image } from "react-bootstrap"
+
+export default function ImageComponent({imageData}) {
+
+    const [liked, setLiked] = useState(false)
+    const [showDescription, setShowDescription] = useState(false)
+
+    const toggleLike = () => {
+        setLiked(!liked)
+    }
+
+    const toggleDescription = () => {
+        setShowDescription(!showDescription)
+    }
+
+    return(
+        <Card className="photoCard">
+            <Card.Img src={imageData.url} alt={`APOD from ${imageData.date}`}/>
+            <Card.Body>
+                <Card.Title>{imageData.title}</Card.Title>
+                { liked 
+                    ? <Image className="likedHeart" src="https://www.downloadclipart.net/large/11072-red-heart-design.png" alt="heart icon"/>
+                    : null
+                }
+                <h5 className="imageDate">{imageData.date}</h5>
+                {/* <Accordion defaultActiveKey="0" flush>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>
+                            Read More */}
+                        {/* </Accordion.Header>
+                        <Accordion.Body> */}
+                {showDescription ? 
+                    <Card.Text>
+                        {imageData.explanation}
+                    </Card.Text>
+                    : null    
+                }
+                <Button variant="link" onClick={() => toggleDescription()}>
+                   {showDescription ? "Hide Description" : "Show Description"}
+                </Button>
+                <Button variant="outline-dark" onClick={() => toggleLike()}>{liked ? "Unlike" : "Like"}</Button>
+            </Card.Body>
+        </Card>
+    )
+}
